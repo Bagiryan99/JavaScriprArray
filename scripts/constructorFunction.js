19595748376585a78159d9684fc33fc9607a6d82
Inna Bagiryan <in.bagiryan25@ail.ru>
2021-02-07 16:41:02 +0400

    ConstructorFunction

---
+++ scripts/constructorFunction.js
@@ -0,0 +1,42 @@
+function User(pName, pAge) {
+    this.name = pName;
+    this.age = pAge;
+    this.displayInfo = function(){
+    return "Hello" +  " "  + this.name;
+    };
+    
+}
+var user1 = new User("Inna", 20);
+//console.log(user1);
+//user1.displayInfo();
+
+function Car(mName, mYear) {
+    this.name  = mName;
+    this.year = mYear;
+    this.getCarInfo = function() {
+        return "Toyota: " + this.name + "  2020: " + this.year;
+        
+    };
+    
+};
+function User(pName, pAge) {
+    this.name = pName;
+    this.age = pAge;
+    this.driveCar = function (car) {   
+        //console.log(this.name + "drives a car " + car.name)
+    };
+    this.displayInfo = function(){
+       // console.log("Name: " + this.name + "; Age: " + this.age);
+    };
+};
+var tom = new User("Luiza ", 26);
+tom.displayInfo();
+var bently = new Car("Bently", 2004);
+tom.driveCar(bently);
+
+//Instanceof operator
+var user = new User("Sofia", 20);
+var isUser = user instanceof User;
+var isCar =  user instanceof  Car;
+console.log(isUser);    // true
+console.log(isCar);     // false
--- scripts/homework.js
+++ scripts/homework.js
@@ -1,16 +1,32 @@
-// ex 1
-function user(string) {
-    return string[0].toUpperCase() + string.slice(1);
+// // ex 1
+// function user(string) {
+//     return string[0].toUpperCase() + string.slice(1);
 
-}
-console.log(user("hello"));
+// }
+// console.log(user("hello"));
 
-//ex 2
- var people = ["Mariam", "Rozi", "David"];
- function arrayClone(arr) {
-     return arr.slice(0);
+// //ex 2 Clone array
+//  var people = ["Mariam", "Rozi", "David"];
+//  function arrayClone(arr) {
+//      return arr.slice(0);
      
- };
+//  };
 
- var arr1 = arrayClone(people);
- console.log(arr1);
+//  var arr1 = arrayClone(people);
+//  console.log(arr1);
+
+//clone array with spread operator
+function cloneArray(arr) {
+    let clonedArr = [...arr];
+    //return clonedArr;
+    
+}
+//console.log(cloneArray([10,20,30,40]));
+
+function cloneArray(arr) {
+    let clonedArr = [].concat(arr);
+   // return clonedArr;
+    
+}
+//console.log(cloneArray([10,20,30,50]));
+
